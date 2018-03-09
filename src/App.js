9ea493abce3ddc64aecac6a7e931b3fe2f15ba78
Willem_Pijl <willemvanderpijl@hotmail.com>
2018-03-09 10:16:35 +0100

    belangrijkste update ooit Roman dit moet je echt pullen

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo} className="App-logo" id="App-logo-2" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
