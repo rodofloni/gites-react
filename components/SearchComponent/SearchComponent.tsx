108db6a8a086f37a3b251da4f5b18096c190e636
rodofloni <romanr@hotmail.nl>
2018-06-01 10:43:16 +0200

    searchcomponent finished
    dd

--- components/SearchComponent/SearchComponent.tsx
+++ components/SearchComponent/SearchComponent.tsx
@@ -16,7 +16,7 @@ public static defaultProps: Partial<SearchProps> = {
             <ul className='search-list'>
                 <form className='simple_form search__form' id='new_search' action='/home/quick_filter' accept-charset='UTF-8' method='post'><input name='utf8' type='hidden' value='&#x2713;' /><input type='hidden' name='_method' value='put' /><input type='hidden' name='authenticity_token' value='zx3003hU3sX1Dj3dF/fsIut8HyjDFQxc6BvStWAz+24t8C3iZ/xg4IQHwOD/wpMPMhMJXwx2FmD8GgIAfZlSoA==' />
                     <li id='search-list-tab-persons'>
-                        <div className='input select optional search_max_persons'><label id='search-label'>Aantal personen</label><select className='select optional select-no-search input--white' data-findex='2' data-placeholder='Selecteer' placeholder='3 personen' name='search[max_persons]' id='search_max_persons'><option value=''></option>
+                        <div className='input select optional search_max_persons'><label id='search-label'>Aantal personen</label><select className='select optional select-no-search input--white' data-findex='2' data-placeholder='Selecteer' placeholder= '3 personen' name='search[max_persons]' id='search-max-persons'><option value=''></option>
                             <option value='1'>1 persoon</option>
                             <option value='2'>2 personen</option>
                             <option value='3'>3 personen</option>
@@ -41,20 +41,20 @@ public static defaultProps: Partial<SearchProps> = {
                         </div>
                     </li>
                     <li id='search-list-tab'>
-                        <div className='search__col search__col--datepicker'>
+                        <div className='search-list-date'>
                             <div data-react-class='AdhocPeriodPicker' data-react-props='{&quot;startDateSelector&quot;:&quot;.search__form .start input&quot;,&quot;endDateSelector&quot;:&quot;.search__form .end input&quot;,&quot;startDate&quot;:null,&quot;endDate&quot;:null}' className='homepage_datepicker'></div>
-                            <div className='start'><div className='input hidden search_start_date'><label id='search-label'>Van: </label><input type='date' name='start-date' /> </div></div>
+                            <div className='start'><div className='input hidden search_start_date'><label id='search-label'>Van: </label><input id='search-list-date-from' type='date' name='start-date' /> </div></div>
                         </div>
                     </li>
                     <li id='search-list-tab'>
-                        <div className='search__col search__col--datepicker'>
+                        <div className='search-list-date'>
                             <div data-react-class='AdhocPeriodPicker' data-react-props='{&quot;startDateSelector&quot;:&quot;.search__form .start input&quot;,&quot;endDateSelector&quot;:&quot;.search__form .end input&quot;,&quot;startDate&quot;:null,&quot;endDate&quot;:null}' className='homepage_datepicker'></div>
-                            <div className='end'><div className='input hidden search_end_date'><label id='search-label'>Tot: </label><input type='date' name='end-date' /> </div></div>
+                            <div className='end'><div className='input hidden search_end_date'><label id='search-label'>Tot: </label><input id='search-list-date-till' type='date' name='end-date' /> </div></div>
                         </div>
                     </li>
                     <li id='search-list-button'>
-                        <div className='search__col search__col--button'>
-                            <button name='button' type='submit' className='search-button' data-label='Vind mijn vakantiehuis'> Vind mijn vakantiehuis</button>
+                        <div className='search-list-button-submit'>
+                            <button name='button' type='submit' className='search-button' data-label='Vind mijn vakantiehuis'> <p id='search-list-button-text'>  Vind mijn vakantiehuis </p> </button>
                         </div>
                     </li>
                 </form>
