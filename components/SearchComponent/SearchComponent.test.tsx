f585c89b489785132c95841081b8fbaa2ddba3d9
WillemvdPijl <willemvanderpijl@hotmail.com>
2018-06-11 13:44:25 +0200

    Ts link fix

--- components/SearchComponent/SearchComponent.test.tsx
+++ components/SearchComponent/SearchComponent.test.tsx
@@ -7,7 +7,6 @@ describe('dateFormat function', () => {
     it('Changes the date to the correct format'), () => {
         const searchcomponent = enzyme.shallow(<SearchComponent vacationType = 'zuipen'/>);
         expect(searchcomponent.instance().dateFormat('1996-1-28')).toEqual('20-11-1996')
-        
         //dateFormat('1996-11-20')).toEqual('20-11-1996')
     };
 });
